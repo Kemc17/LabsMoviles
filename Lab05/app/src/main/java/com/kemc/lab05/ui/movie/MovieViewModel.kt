eeea95f3e14d745eb8e8df954d5f8eb7d32b7124
Kemc17 <00129421@uca.edu.sv>
2023-05-04 12:00:16 -0600

    Add lab07

--- Lab05/app/src/main/java/com/kemc/lab05/ui/movie/MovieViewModel.kt
+++ Lab05/app/src/main/java/com/kemc/lab05/ui/movie/MovieViewModel.kt
@@ -1,5 +1,6 @@
 package com.kemc.lab05.ui.movie
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.initializer
@@ -8,17 +9,68 @@ import com.kemc.lab05.MovieReviewerApplication
 import com.kemc.lab05.data.model.MovieModel
 import com.kemc.lab05.repositories.MovieRepository
 
-class MovieViewModel(private val repository: MovieRepository) : ViewModel() {
+class MovieViewModel(private val repository: MovieRepository):ViewModel() {
+    var name = MutableLiveData("")
+    var category = MutableLiveData("")
+    var description = MutableLiveData("")
+    var qualification = MutableLiveData("")
+    var status = MutableLiveData("")
+
     fun getMovies() = repository.getMovies()
 
     fun addMovies(movie: MovieModel) = repository.addMovies(movie)
 
-    companion object {
+    private fun validateData(): Boolean{
+        when{
+            name.value.isNullOrEmpty() -> return false
+            category.value.isNullOrEmpty() -> return false
+            description.value.isNullOrEmpty() -> return false
+            qualification.value.isNullOrEmpty() -> return false
+        }
+        return true
+    }
+
+    fun createMovie(){
+        if (!validateData()){
+            status.value = WRONG_DATA
+            return
+        }
+
+        val newMoview = MovieModel(
+            name.value.toString(),
+            category.value.toString(),
+            description.value.toString(),
+            qualification.value.toString()
+        )
+
+        addMovies(newMoview)
+        clearData()
+        status.value = MOVIE_CREATED
+    }
+
+    fun clearStatus(){
+        status.value = INACTIVE
+    }
+
+    fun clearData(){
+        name.value = ""
+        category.value = ""
+        description.value = ""
+        qualification.value = ""
+    }
+
+    companion object{
         val Factory = viewModelFactory {
             initializer {
                 val app = this[APPLICATION_KEY] as MovieReviewerApplication
                 MovieViewModel(app.movieRepository)
             }
         }
+
+        const val MOVIE_CREATED = "Movie created"
+        const val WRONG_DATA = "Wrong data"
+        const val INACTIVE = ""
     }
+
+
 }
--- Lab05/app/src/main/java/com/kemc/lab05/ui/movie/NewMovieFragment.kt
+++ Lab05/app/src/main/java/com/kemc/lab05/ui/movie/NewMovieFragment.kt
@@ -12,55 +12,86 @@ import com.google.android.material.textfield.TextInputEditText
 import com.kemc.lab05.R
 import androidx.navigation.fragment.findNavController
 import com.kemc.lab05.data.model.MovieModel
+import com.kemc.lab05.databinding.FragmentNewMovieBinding
 
 class NewMovieFragment : Fragment() {
     private val viewModel: MovieViewModel by activityViewModels{
         MovieViewModel.Factory
     }
 
-    private lateinit var name: TextInputEditText
-    private lateinit var category: TextInputEditText
-    private lateinit var description: TextInputEditText
-    private lateinit var calification: TextInputEditText
-    private lateinit var action: Button
+    private lateinit var binding: FragmentNewMovieBinding
+
+//    private lateinit var name: TextInputEditText
+//    private lateinit var category: TextInputEditText
+//    private lateinit var description: TextInputEditText
+//    private lateinit var calification: TextInputEditText
+//    private lateinit var action: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_movie, container, false)
+    ): View {
+    binding = FragmentNewMovieBinding.inflate(inflater, container, false)
+        return binding.root
+
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_new_movie, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        bind(view)
+        setViewModel()
+        observeStatus()
+//        bind(view)
+//
+//        action.setOnClickListener{
+//            createMovie()
+//        }
+    }
 
-        action.setOnClickListener{
-            createMovie()
+    private fun setViewModel() {
+        binding.viewmodel = viewModel
+    }
+
+    private fun observeStatus(){
+        viewModel.status.observe(viewLifecycleOwner) { status ->
+            when{
+                status.equals(MovieViewModel.MOVIE_CREATED) -> {
+                    Log.d("APP_TAG", status)
+                    Log.d("APP_TAG", viewModel.getMovies().toString())
+                    viewModel.clearData()
+                    viewModel.clearStatus()
+                    findNavController().popBackStack()
+                }
+                status.equals(MovieViewModel.WRONG_DATA) -> {
+                    Log.d("APP_TAG", status)
+                    viewModel.clearStatus()
+                    //findNavController().popBackStack()
+                }
+            }
         }
     }
 
-    private fun bind(view: View){
-        name = view.findViewById(R.id.text_inputName)
-        category = view.findViewById(R.id.text_inputCategory)
-        description = view.findViewById(R.id.text_inputDescription)
-        calification = view.findViewById(R.id.text_inputCalification)
-        action = view.findViewById(R.id.button)
-    }
-
-    private fun createMovie(){
-        val newMoview = MovieModel(
-            name.text.toString(),
-            category.text.toString(),
-            description.text.toString(),
-            calification.text.toString()
-        )
-        viewModel.addMovies(newMoview)
-
-        Log.d("APP TAG", viewModel.getMovies().toString())
-
-        findNavController().popBackStack()
-    }
+//    private fun bind(view: View){
+//        name = view.findViewById(R.id.text_inputName)
+//        category = view.findViewById(R.id.text_inputCategory)
+//        description = view.findViewById(R.id.text_inputDescription)
+//        calification = view.findViewById(R.id.text_inputCalification)
+//        action = view.findViewById(R.id.button)
+//    }
+//
+//    private fun createMovie(){
+//        val newMoview = MovieModel(
+//            name.text.toString(),
+//            category.text.toString(),
+//            description.text.toString(),
+//            calification.text.toString()
+//        )
+//        viewModel.addMovies(newMoview)
+//
+//        Log.d("APP TAG", viewModel.getMovies().toString())
+//
+//        findNavController().popBackStack()
+//    }
 }
